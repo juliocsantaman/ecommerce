5d5959258633f10536a4b70c52b839e562c1e199
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-06-24 12:09:04 -0500

    Adding product to the cart

--- src/app/domains/products/models/product.model.ts
+++ src/app/domains/products/models/product.model.ts
@@ -1,5 +1,7 @@
 export interface Product {
+    id: number;
     img: string;
     title: string;
     price: number;
+    createdAt: string;
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../models/product.model';
 
@@ -11,45 +11,65 @@ import { Product } from '../../models/product.model';
 })
 export class ListComponent implements OnInit {
 
-  products: Product[] = [];
+  products = signal<Product[]>([]);
+  cart = signal<Product[]>([]);
+
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-      this.getProducts();
+    this.getProducts();
   }
 
   getProducts(): void {
-    this.products = 
-    [
-      {
-        img: 'https://picsum.photos/640/640',
-        title: 'Product 1',
-        price: 3000
-      },
-      {
-        img: 'https://picsum.photos/640/640',
-        title: 'Product 2',
-        price: 4000
-      },
-      {
-        img: 'https://picsum.photos/640/640',
-        title: 'Product 3',
-        price: 5000
-      },
-      {
-        img: 'https://picsum.photos/640/640',
-        title: 'Product 4',
-        price: 6000
-      },
-    ];
+    this.products.set(
+      [
+        {
+          id: Date.now(),
+          img: 'https://picsum.photos/640/640',
+          title: 'Product 1',
+          price: 3000,
+          createdAt: new Date().toISOString()
+        },
+        {
+          id: Date.now(),
+          img: 'https://picsum.photos/640/640',
+          title: 'Product 2',
+          price: 4000,
+          createdAt: new Date().toISOString()
+        },
+        {
+          id: Date.now(),
+          img: 'https://picsum.photos/640/640',
+          title: 'Product 3',
+          price: 5000,
+          createdAt: new Date().toISOString()
+        },
+        {
+          id: Date.now(),
+          img: 'https://picsum.photos/640/640',
+          title: 'Product 4',
+          price: 6000,
+          createdAt: new Date().toISOString()
+        },
+      ]
+    );
   }
 
   addToCart(product: Product): void {
     console.group('addToCart');
     console.log('product', product);
+    this.cart.update((previuosState) => [...previuosState, product]);
+    console.log(' this.cart()',  this.cart());
+    console.groupEnd();
+  }
+
+  deleteProductFromCartTest(index: number): void {
+    console.group('deleteProductFromCartTest');
+    console.log('product', index);
+    this.cart.update((previuosState) => previuosState.filter((product, position) => position != index));
     console.groupEnd();
   }
 
