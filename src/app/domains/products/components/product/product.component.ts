8b543e4e053db58ae2fe20d05a889d2837ffedad
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-06-26 05:27:56 -0500

    Using pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+import { UpperCasePipe, CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  //imports: [UpperCasePipe, CurrencyPipe, DatePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
